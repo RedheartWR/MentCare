b9a2f778a93ec5d74ac01a08db6c14d160710d5a
Redheart <alisa-book@yandex.ru>
2019-05-09 01:26:46 +0300

    Weird version of graphs (no scaling)

--- app/src/main/java/com/example/tanya_green/mentcare/GraphicsActivity.java
+++ app/src/main/java/com/example/tanya_green/mentcare/GraphicsActivity.java
@@ -1,5 +1,8 @@
 package com.example.tanya_green.mentcare;
 
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
@@ -11,13 +14,15 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
+
+import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
@@ -32,18 +37,28 @@ import java.util.Comparator;
 public class GraphicsActivity extends AppCompatActivity {
 
     CheckBox sleepCheckBox, appetiteCheckBox, moodCheckBox;
-    GraphView graph;
     DataSymptoms mDataSymptoms;
     String path = "data.ser";
+    GraphView graph;
+    LineGraphSeries<DataPoint> series;
+    //SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphics);
-        graph = findViewById(R.id.graph);
         sleepCheckBox = findViewById(R.id.sleepCheckBox);
         moodCheckBox = findViewById(R.id.moodCheckBox);
         appetiteCheckBox = findViewById(R.id.appetiteCheckBox);
+        graph = (GraphView) findViewById(R.id.graph);
+        /*graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter(){
+            @Override
+            public String formatLabel(double value, boolean isValueX){
+                if (isValueX)
+                    return sdf.format(new Date((long)value));
+                return super.formatLabel(value, isValueX);
+            }
+        });*/
         try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path)))
         {
             mDataSymptoms = (DataSymptoms) ois.readObject();
@@ -54,11 +69,11 @@ public class GraphicsActivity extends AppCompatActivity {
         catch(Exception ex){
             mDataSymptoms = new DataSymptoms();
             HashMap<String, Integer> symptoms = new HashMap<String, Integer>();
-            symptoms.put("sleep", 1);
+            symptoms.put("sleep", 4);
             symptoms.put("mood", 3);
             mDataSymptoms.AddDaySymptoms(new GregorianCalendar(), symptoms);
             symptoms.clear();
-            symptoms.put("sleep", 1);
+            symptoms.put("sleep", 10);
             mDataSymptoms.AddDaySymptoms(new GregorianCalendar(2019, 4, 3), symptoms);
         }
     }
@@ -98,39 +113,58 @@ public class GraphicsActivity extends AppCompatActivity {
             Map<Calendar, Integer> graphData = mDataSymptoms.GetSymptomStatistic(symptom);
             if (graphData == null) return;
             ArrayList<Date> dateArray = new ArrayList<Date>();
-            ArrayList<Integer> intArray = new ArrayList<>();
             Toast.makeText(getApplicationContext(),
                     ""+graphData.size(),
                     Toast.LENGTH_SHORT).show();
             for(Map.Entry<Calendar, Integer> item : graphData.entrySet()){
                 dateArray.add(item.getKey().getTime());
-                intArray.add(item.getValue());
             }
             Toast.makeText(getApplicationContext(),
                     ""+dateArray.size(),
                     Toast.LENGTH_SHORT).show();
-            DataPoint[] dataPoint = new DataPoint[dateArray.size()];
             dateArray.sort(new Comparator<Date>(){
                 public int compare(Date d1, Date d2) {
                     return d1.compareTo(d2);
                 }});
-
-            for (int i = 0; i < dateArray.size(); i++)
-                dataPoint[i] = new DataPoint(dateArray.get(i), intArray.get(i));
             Toast.makeText(getApplicationContext(),
-                    "there",
+                    "state1",
                     Toast.LENGTH_SHORT).show();
-            LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPoint);
+            DataPoint[] dataPoints = new DataPoint[dateArray.size()];
+            for (int i = 0; i < dataPoints.length; i++){
+                Calendar calendar = new GregorianCalendar();
+                calendar.setTime(dateArray.get(i));
+                dataPoints[i] = new DataPoint(dateArray.get(i), graphData.get(calendar));
+            }
+            Toast.makeText(getApplicationContext(),
+                    "state2",
+                    Toast.LENGTH_SHORT).show();
+            series = new LineGraphSeries<>(dataPoints);
+            // styling series
+            series.setColor(Color.GREEN);
+            series.setDrawDataPoints(true);
+            series.setDataPointsRadius(10);
+            series.setThickness(8);
+
+            // custom paint to make a dotted line
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(10);
+            paint.setPathEffect(new DashPathEffect(new float[]{8, 5}, 0));
+            series.setCustomPaint(paint);
+
+            // activate horizontal zooming and scrolling
+            graph.getViewport().setScalable(true);
+
+// activate horizontal scrolling
+            graph.getViewport().setScrollable(true);
+
+// activate horizontal and vertical zooming and scrolling
+            graph.getViewport().setScalableY(true);
+
+// activate vertical scrolling
+            graph.getViewport().setScrollableY(true);
+
             graph.addSeries(series);
-            // set date label formatter
-            graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
-            graph.getGridLabelRenderer().setNumHorizontalLabels(3); // only 4 because of the space
-
-            graph.getViewport().setMinX(dateArray.get(0).getTime());
-            graph.getViewport().setMaxX(dateArray.get(dateArray.size() - 1).getTime());
-            graph.getViewport().setXAxisBoundsManual(true);
-
-            graph.getGridLabelRenderer().setHumanRounding(false);
             Toast.makeText(getApplicationContext(),
                     "DONE",
                     Toast.LENGTH_SHORT).show();
